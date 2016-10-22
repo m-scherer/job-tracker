b89c34bc85f76d4bf4858123628edad9aece5b70
Michael Scherer <michael.c.scherer@gmail.com>
2016-10-22 17:46:39 -0600

    refactor job model methods

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
 
   def index
     @companies_location = Company.location_job_count
-    @top_three_companies_interest = Job.top_3_interest_by_company
+    @top_three_companies_interest = Job.sorted_average_interest
     @level_of_interest =  Job.count_level_of_interest
   end
 
--- app/models/job.rb
+++ app/models/job.rb
@@ -9,26 +9,11 @@ class Job < ActiveRecord::Base
   end
 
   def self.get_average_interest
-    Job.group(:company_id).average(:level_of_interest)
+    Job.joins(:company).group(:name).average(:level_of_interest)
   end
 
-  def self.sort_average_interest
+  def self.sorted_average_interest
     get_average_interest.sort_by { |k,v| v }.reverse
   end
-
-  def self.top_3_interest_by_company
-    sort_average_interest.map do |k,v|
-      "#{find_company_name(k)}: #{v.to_f}"
-    end[0..2]
-  end
-
-  def self.find_company_name(k)
-    Company.find(k).name
-  end
-
-  def self.location_job_count
-      Company.joins(:jobs).group(:city).count
-  end
-
 
 end
