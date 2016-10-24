fed0748c646d29098f0df5b051a5e6e14f550ca7
Michael Scherer <michael.c.scherer@gmail.com>
2016-10-24 08:57:09 -0600

    fix errors associated with styling

--- spec/features/dashboard/user_sees_dashboard_data_spec.rb
+++ spec/features/dashboard/user_sees_dashboard_data_spec.rb
@@ -14,26 +14,25 @@ describe 'User sees dashboard data' do
   scenario 'user sees level of interest count' do
     visit dashboard_index_path
     within('#level_of_interest') do
-      expect(page).to have_content("#{job1.level_of_interest} (1)")
-      expect(page).to have_content("#{job2.level_of_interest} (1)")
-      expect(page).to have_content("#{job3.level_of_interest} (1)")
-      expect(page).to have_content("#{job4.level_of_interest} (1)")
+      expect(page).to have_content("1 #{job1.level_of_interest}")
+      expect(page).to have_content("1 #{job2.level_of_interest}")
+      expect(page).to have_content("1 #{job3.level_of_interest}")
+      expect(page).to have_content("1 #{job4.level_of_interest}")
     end
   end
 
   scenario 'user sees top 3 interesting companies' do
     visit dashboard_index_path
     within('#interest_companies') do
-      expect(page).to have_content("#{company1.name} (100.0)")
-      expect(page).to have_content("#{company2.name} (90.0)")
-      expect(page).to have_content("#{company3.name} (80.0)")
-    end
-  end
+      expect(page).to have_content("100.0 #{company1.name}")
+      expect(page).to have_content("90.0 #{company2.name}")
+      expect(page).to have_content("80.0 #{company3.name}")
+    end  end
 
   scenario 'user sees jobs in each city' do
     visit dashboard_index_path
     within('#city_jobs') do
-      expect(page).to have_content("Here (4)")
+      expect(page).to have_content("4 Here")
     end
   end
 
